f2fe454012beda6f618644be1816378c270c71d0
ranthor <avlex@gmx.net>
2014-02-16 23:49:34 +0100

    Added config example

---
+++ NeoPixelServerConfig.example.h
@@ -0,0 +1,9 @@
+// Wifi setup
+#define WLAN_SECURITY  WLAN_SEC_WPA2
+#define WLAN_SSID  "Niflheim"
+#define WLAN_PASS  "$-1uah1O"
+
+// NeoPixel config
+#define NEOPIXEL_COUNT          16
+#define NEOPIXEL_BRIGHTNESS     192
+#define NEOPIXEL_MAX_BRIGHTNESS 255
