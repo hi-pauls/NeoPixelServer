b5178fdfafffbc9e20b40760bbee2d1773c887d5
Paul <pschulze@helgames.com>
2018-03-25 18:25:36 +0200

    changed config to contain all options

--- NeoPixelServerConfig.example.h
+++ NeoPixelServerConfig.example.h
@@ -1,9 +1,31 @@
 // Wifi setup
 #define WLAN_SECURITY  WLAN_SEC_WPA2
-#define WLAN_SSID  "Niflheim"
-#define WLAN_PASS  "$-1uah1O"
+#define WLAN_SSID  "ssid"
+#define WLAN_PASS  "pass"
 
 // NeoPixel config
 #define NEOPIXEL_COUNT          16
 #define NEOPIXEL_BRIGHTNESS     192
+#define NEOPIXEL_USB_BRIGHTNESS 192
 #define NEOPIXEL_MAX_BRIGHTNESS 255
+
+// Start with the light enabled
+//#define START_ENABLED
+
+// Serial settings
+//#define NO_SERIAL
+//#define SERIAL_WAIT
+
+// Disable some or all effects
+//#define NO_EFFECTS
+//#define NO_FADE
+//#define NO_RAINBOW
+//#define NO_FLICKER
+//#define NO_FIREWORKS
+//#define NO_RUN
+//#define NO_CYLON
+//#define NO_SPECTRUM
+
+// Reduce the website size
+//#define WEBSITE_NONE
+//#define WEBSITE_SHORT
